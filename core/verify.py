80726da72113fcf569e152d3acad11599f1a022b
zeinlol <nickolaiborshchov@gmail.com>
2022-09-19 16:53:57 +0100

    refactor code and saving to json

--- core/verify.py
+++ core/verify.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import re
+
 from module import globals
-from module.time import now
 from module.color import color
 from module.output import output
+from module.time import now
 
 
 class Verification:
@@ -19,23 +20,22 @@ class Verification:
             vul_type = vul_info["vul_type"]
             vul_numb = vul_info["vul_numb"]
             info = vul_info["prt_info"]
-            if result == "PoCSuCCeSS":  # 存在漏洞时候输出以下内容
-                print(now.timed(de=delay) + color.green("[+] The target is " + prt_name + " " + info))
+            if result == "PoCSuCCeSS":
+                print(now.timed(de=delay) + color.green(f"[+] The target is {prt_name} {info}"))
+
                 # 丢给output模块判断是否输出文件
-                output("json", vul_info)
-                output("text", "--> [名称:" + vul_name + "] [编号:" + vul_numb + "] [类型:" + vul_type + "] " + info)
+                output("text", f"--> [名称:{vul_name}] [编号:{vul_numb}] [类型:{vul_type}] {info}")
             elif result == "PoC_MaYbE":
-                print(now.timed(de=delay) + color.green("[?] The target maybe " + prt_name + " " + info))
+                print(now.timed(de=delay) + color.green(f"[?] The target maybe {prt_name} {info}"))
                 # 丢给output模块判断是否输出文件
-                output("json", vul_info)
-                output("text", "--> [名称:" + vul_name + "] [编号:" + vul_numb + "] [类型:" + vul_type + "] " + info)
-            else:  # 否则就是没有洞
-                if debug == "debug":
-                    print(now.timed(de=delay) + color.magenta("[-] The target no " + color.magenta(prt_name)))
-                else:
-                    print("\r{0}{1}{2}".format(now.timed(de=delay),
-                                               color.magenta("[-] The target no "),
-                                               color.magenta(prt_name)), end="                           \r", flush=True)
+                output("text", f"--> [名称:{vul_name}] [编号:{vul_numb}] [类型:{vul_type}] {info}")
+            elif debug == "debug":
+                print(now.timed(de=delay) + color.magenta(f"[-] The target no {color.magenta(prt_name)}"))
+                # 丢给output模块判断是否输出文件
+            else:
+                print(f'\r{now.timed(de=delay)}{color.magenta("[-] The target no ")}{color.magenta(prt_name)}',
+                      end="                           \r", flush=True)
+            output("json", vul_info)
         except IndexError as error:
             print(now.timed(de=0) + color.red("[ERROR] " + error.__traceback__.tb_frame.f_globals['__file__']
                                               + " " + str(error.__traceback__.tb_lineno)))
@@ -56,38 +56,33 @@ class Verification:
         delay = globals.get_value("DELAY")  # 获取全局变量DELAY
         debug = globals.get_value("DEBUG")  # 获取全局变量DEBUG
         if debug == "debug":
-            print(now.timed(de=delay) + color.red_warn() +
-                  color.cyan(" " + prt_name + " check failed because timeout !!!"))
+            print(
+                (now.timed(de=delay) + color.red_warn() + color.cyan(f" {prt_name} check failed because timeout !!!")))
+
         else:
-            print("\r{0}{1}{2}".format(now.timed(de=delay),
-                                       color.red_warn(),
-                                       color.cyan(" " + prt_name + " connection timeout !!!")),
-                  end="                            \r",
-                  flush=True)
+            print(f"\r{now.timed(de=delay)}{color.red_warn()}{color.cyan(f' {prt_name} connection timeout !!!')}",
+                  end="                            \r", flush=True)
+
     @staticmethod
     def connection_print(prt_name):
         delay = globals.get_value("DELAY")  # 获取全局变量DELAY
         debug = globals.get_value("DEBUG")  # 获取全局变量DEBUG
         if debug == "debug":
-            print(now.timed(de=delay) + color.red_warn() +
-                  color.cyan(" " + prt_name + " check failed because unable to connect !!!"))
+            print(now.timed(de=delay) + color.red_warn() + color.cyan(
+                f" {prt_name} check failed because unable to connect !!!"))
+
         else:
-            print("\r{0}{1}{2}".format(now.timed(de=delay),
-                                       color.red_warn(),
-                                       color.cyan(" " + prt_name + " connection failed !!!")),
-                  end="                            \r",
-                  flush=True)
+            print(f"\r{now.timed(de=delay)}{color.red_warn()}{color.cyan(f' {prt_name} connection failed !!!')}",
+                  end="                            \r", flush=True)
 
     @staticmethod
     def error_print(prt_name):
         delay = globals.get_value("DELAY")  # 获取全局变量DELAY
         debug = globals.get_value("DEBUG")  # 获取全局变量DEBUG
         if debug == "debug":
-            print(now.timed(de=delay) + color.magenta("[-] The target no " + color.magenta(prt_name)))
+            print(now.timed(de=delay) + color.magenta(f"[-] The target no {color.magenta(prt_name)}"))
         else:
-            print("\r{0}{1}{2}".format(now.timed(de=delay),
-                                       color.magenta("[-] The target no "),
-                                       color.magenta(prt_name)),
+            print(f"\r{now.timed(de=delay)}{color.magenta('[-] The target no ')}{color.magenta(prt_name)}",
                   end="                            \r",
                   flush=True)
 
@@ -96,8 +91,5 @@ verify = Verification()
 
 
 def misinformation(req, md):  # 用来处理echo被错误返回时的误报，代码小巧作用甚大
-    bad = "echo.{0,10}" + md    # 使用正则来应对复杂的编码情况
-    if(re.search(bad, req) != None):
-        return "misinformation"
-    else:
-        return req
+    bad = "echo.{0,10}" + md  # 使用正则来应对复杂的编码情况
+    return "misinformation" if re.search(bad, req) is not None else req
--- module/output.py
+++ module/output.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import re
-import time
 import json
 import os.path
-from module.time import now
-from module.color import color
-from module import globals
+import re
+import time
 from urllib.parse import urlparse
+
+from module import globals
+from module.color import color
+from module.time import now
 
 
 def output(types, item):
@@ -16,10 +17,10 @@ def output(types, item):
         o_json = globals.get_value("O_JSON")
         if o_text and types == "text":
             output_text(o_text, item)
-        elif o_json and types == "json":
+        if o_json and types == "json":
             output_json(o_json, item)
     except Exception as error:
-        print(now.timed(de=0) + color.red("[ERROR] " + error.__traceback__.tb_frame.f_globals['__file__']
+        print(now.timed(de=0) + color.red(f"[ERROR] {error} " + error.__traceback__.tb_frame.f_globals['__file__']
                                           + " " + str(error.__traceback__.tb_lineno)))
 
 
@@ -29,126 +30,127 @@ def output_text(filename, item):
 
 
 def output_json(filename, data):
+    print(f'{data=}')
     vul_data = data["vul_data"]
-    raw_data = []
+    json_results = []
+    vul_path, vul_requ, vul_resp = parse_data(vul_data)
     try:
-        if r">_<" in vul_data:
-            vul_requ = vul_data
-            vul_resp = vul_data
-            vul_path = ""
-        else:
-            raw_data.append(vul_data)
-            vul_requ = re.findall(r'([\s\S]*)\r\n> HTTP/', raw_data[0])[0]
-            vul_requ = vul_requ.replace("< ", "")
-            vul_resp = re.findall(r'\r\n> HTTP/([\s\S]*)', raw_data[0])[0]
-            vul_resp = "HTTP/" + vul_resp.replace("> ", "")
-            vul_path = re.findall(r' /(.*) HTTP', raw_data[0])[0]
-    except Exception as error:
-        print(now.timed(de=0) + color.red("[ERROR] " + error.__traceback__.tb_frame.f_globals['__file__']
-                                          + " " + str(error.__traceback__.tb_lineno)))
-        vul_path = ""
-        vul_requ = ""
-        vul_resp = ""
-
-    try:
-        vul_urls = data["vul_urls"]
-        host_port = urlparse(vul_urls)
-        vul_host = host_port.hostname
-        vul_port = host_port.port
-        # vul_u = vul_host + ":" + str(vul_port)
-        if vul_port is None:
-            if r"https://" in vul_urls:
-                vul_port = 443
-            elif r"http://" in vul_urls:
-                vul_port = 80
-        if r"https://" in vul_urls:
-            if vul_port is not None:
+        if vul_data:
+            vul_urls = data["vul_urls"]
+            host_port = urlparse(vul_urls)
+            vul_host = host_port.hostname
+            vul_port = host_port.port
+            # vul_u = vul_host + ":" + str(vul_port)
+            if vul_port is None:
+                if r"https://" in vul_urls:
+                    vul_port = 443
+                elif r"http://" in vul_urls:
+                    vul_port = 80
+            if r"https://" in vul_urls and vul_port is not None:
                 vul_u = f"https://{vul_host}:{str(vul_port)}/{vul_path}"
-            else:
+            elif r"https://" in vul_urls:
                 vul_u = f"https://{vul_host}/{vul_path}"
-        elif r"http://" in vul_urls:
-            if vul_port is not None:
+            elif r"http://" in vul_urls and vul_port is not None:
                 vul_u = f"http://{vul_host}:{str(vul_port)}/{vul_path}"
             else:
                 vul_u = f"http://{vul_host}/{vul_path}"
-        else:
-            vul_u = f"http://{vul_host}/{vul_path}"
-        prt_name = data["prt_name"]
-        vul_payd = data["vul_payd"]
-        vul_type = data["vul_type"]
-        vul_auth = data["cre_auth"]
-        vul_desc = data["vul_name"]
-        vul_date = int(round(time.time() * 1000))
-        json_result = []
-        json_data = {
-            "create_time": vul_date,
-            "detail": {
-                "author": vul_auth,
-                "description": vul_desc,
-                "host": vul_host,
-                "param": {},
-                "payload": vul_payd,
-                "port": vul_port,
-                "request": vul_requ,
-                "response": vul_resp,
-                "url": vul_u
-            },
-            "plugin": prt_name,
-            "target": {
-                "url": vul_urls
-            },
-            "vuln_class": vul_type
-        }
-        json_result.append(json_data)
-
-        def write_json(obj):
-            item_list = []
-            if os.path.isfile(filename):
-                with open(filename, 'r') as f:
-                    load_dict = json.load(f)
-                    num_item = len(load_dict)
-                    for i in range(num_item):
-                        create_time = load_dict[i]['create_time']
-                        author = load_dict[i]['detail']['author']
-                        description = load_dict[i]['detail']['description']
-                        host = load_dict[i]['detail']['host']
-                        param = load_dict[i]['detail']['param']
-                        payload = load_dict[i]['detail']['payload']
-                        port = load_dict[i]['detail']['port']
-                        request = load_dict[i]['detail']['request']
-                        response = load_dict[i]['detail']['response']
-                        url_1 = load_dict[i]['detail']['url']
-                        plugin = load_dict[i]['plugin']
-                        url_2 = load_dict[i]['target']['url']
-                        vuln_class = load_dict[i]['vuln_class']
-                        json_dict = {
-                            "create_time": create_time,
-                            "detail": {
-                                "author": author,
-                                "description": description,
-                                "host": host,
-                                "param": param,
-                                "payload": payload,
-                                "port": port,
-                                "request": request,
-                                "response": response,
-                                "url": url_1
-                            },
-                            "plugin": plugin,
-                            "target": {
-                                "url": url_2
-                            },
-                            "vuln_class": vuln_class
-                        }
-                        item_list.append(json_dict)
-            else:
-                with open(filename, 'w', encoding='utf-8') as f2:
-                    json.dump(json_result, f2, indent=4, ensure_ascii=False)
-            item_list.append(obj)
-            with open(filename, 'w', encoding='utf-8') as f2:
-                json.dump(item_list, f2, indent=4, ensure_ascii=False)
-
-        write_json(json_data)
+            prt_name = data["prt_name"]
+            vul_payd = data["vul_payd"]
+            vul_type = data["vul_type"]
+            vul_desc = data["vul_name"]
+            vul_date = int(round(time.time() * 1000))
+            json_data = {
+                "create_time": vul_date,
+                "detail": {
+                    "description": vul_desc,
+                    "host": vul_host,
+                    "param": {},
+                    "payload": vul_payd,
+                    "port": vul_port,
+                    "request": vul_requ,
+                    "response": vul_resp,
+                    "url": vul_u
+                },
+                "plugin": prt_name,
+                "target": {
+                    "url": vul_urls
+                },
+                "vuln_class": vul_type
+            }
+            json_results.append(json_data)
+        write_json(json_results, file_name=filename)
     except Exception as error:
-        print(now.timed(de=0) + color.red("[ERROR] " + error.__traceback__.tb_frame.f_globals['__file__']
+        print(now.timed(de=0) + color.red(f"[ERROR] {error} " + error.__traceback__.tb_frame.f_globals['__file__']
                                           + " " + str(error.__traceback__.tb_lineno)))
+
+
+def write_json(obj, file_name):
+    item_list = []
+    try:
+        if os.path.isfile(file_name):  # keep data from result file.
+            with open(file_name, 'r') as f:
+                try:
+                    load_dict = json.load(f)
+                except ValueError:
+                    load_dict = {}
+                for i in range(len(load_dict)):
+                    create_time = load_dict[i]['create_time']
+                    description = load_dict[i]['detail']['description']
+                    host = load_dict[i]['detail']['host']
+                    param = load_dict[i]['detail']['param']
+                    payload = load_dict[i]['detail']['payload']
+                    port = load_dict[i]['detail']['port']
+                    request = load_dict[i]['detail']['request']
+                    response = load_dict[i]['detail']['response']
+                    url_1 = load_dict[i]['detail']['url']
+                    plugin = load_dict[i]['plugin']
+                    url_2 = load_dict[i]['target']['url']
+                    vuln_class = load_dict[i]['vuln_class']
+                    json_dict = {
+                        "create_time": create_time,
+                        "detail": {
+                            "description": description,
+                            "host": host,
+                            "param": param,
+                            "payload": payload,
+                            "port": port,
+                            "request": request,
+                            "response": response,
+                            "url": url_1
+                        },
+                        "plugin": plugin,
+                        "target": {
+                            "url": url_2
+                        },
+                        "vuln_class": vuln_class
+                    }
+                    item_list.append(json_dict)
+        item_list.append(*obj)
+        with open(file_name, 'w', encoding='utf-8') as f2:
+            json.dump(item_list, f2, indent=4, ensure_ascii=False)
+    except Exception as error:
+        print(now.timed(de=0) + color.red(f"[ERROR] {error} " + error.__traceback__.tb_frame.f_globals['__file__']
+                                          + " " + str(error.__traceback__.tb_lineno)))
+
+
+def parse_data(vul_data) -> (str, str, str):
+    if not vul_data or vul_data == 'null':
+        return '', '', ''
+    vul_path = ""
+    request = ""
+    response = ""
+    try:
+        if r">_<" in vul_data:
+            request = vul_data
+            response = vul_data
+        else:
+            request = re.findall(r'([\s\S]*)\r\n> HTTP/', vul_data)[0]
+            request = request.replace("< ", "")
+            response = re.findall(r'\r\n> HTTP/([\s\S]*)', vul_data)[0]
+            response = f'HTTP/{response.replace("> ", "")}'
+            vul_path = re.findall(r' /(.*) HTTP', vul_data)[0]
+    except Exception as error:
+        print(now.timed(de=0) + color.red(f"[ERROR] {error} " + error.__traceback__.tb_frame.f_globals['__file__']
+                                          + " " + str(error.__traceback__.tb_lineno)))
+
+    return vul_path, request, response
